5bf96be6a8490ab785ff41197bd34091d50b9caf
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-04-21 15:05:59 +0100

    added pulsing css effect on homepage

--- src/components/Homepage/homepage-components/IntroductionText/IntroductionText.jsx
+++ src/components/Homepage/homepage-components/IntroductionText/IntroductionText.jsx
@@ -4,7 +4,7 @@ const IntroductionText = () => {
   return (
     <div className="introduction">
       <h1>
-        <span className="introduction__name">giorgi</span><span className="introduction__is--span">.is()</span>
+        <span className="introduction__name">giorgi</span><div className="introduction__dot"></div><span className="introduction__is--span">is()</span>
         <br />
         <p className="introduction__h5">Giorgi Gutsaev</p>
         <p className="introduction__dev--span">
