598ac3a2bf8830964c72f1b38b72e227dd2fa837
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-04-23 12:36:49 +0100

    added the blurry functionality when burger menu is active on Contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,54 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import "./Contact.css";
 import { useEffect, useState, useContext, useRef } from "react";
 import { motion } from "framer-motion";
 import ContactIntroduction from "./contact-components/ContactIntroduction/ContactIntroduction";
 import ContactForm from "./contact-components/ContactForm/ContactForm";
-
+import Paperplane from "./contact-components/AtomicStudent/AtomicStudent";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import MenuContext from "../../context/MenuContext";
 import BlurContext from "../../context/BlurContext";
-import Paperplane from "./contact-components/AtomicStudent/AtomicStudent";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Contact = () => {
+  const [contactWindowWidth, setContactWindowWidth] = useState("");
+  const { isBlurred } = useContext(BlurContext);
+  const { isClicked } = useContext(MenuContext);
+
+  const contactRef = useRef(null);
+
+  // useEffect hook to remove blurriness, IF the burger menu is not clicked to hide it on the (Contact page)
+  useEffect(() => {
+    const updateContactWindowWidth = () => {
+      const newProjectWidth = window.innerWidth;
+      setContactWindowWidth(newProjectWidth);
+    };
+
+    window.addEventListener("resize", updateContactWindowWidth);
+
+    const contactRefCurrent = contactRef.current;
+    const contactRefClass = contactRefCurrent.classList;
+
+    if (isClicked && contactWindowWidth > 767) {
+      contactRefClass.remove("darken");
+    }
+
+    if (contactRefClass.contains("darken") && window.innerWidth > 767) {
+      contactRefClass.remove("darken");
+    }
+
+    // So the user always starts from the top once navigated to Contact page
+    window.scrollTo(0, 0);
+
+    // cleanup function
+    return () => {
+      window.removeEventListener("resize", updateContactWindowWidth);
+    };
+  }, [contactWindowWidth]);
+
   useEffect(() => {
     const contactIntroduction = gsap.from(".contact-introduction", {
       opacity: 0,
@@ -45,9 +80,12 @@ const Contact = () => {
       transition={{ ease: "easeOut", duration: 1 }}
       exit={{ opacity: 0 }}
     >
-      <div className="contact">
+      <div
+        ref={contactRef}
+        className={isBlurred ? "contact darken" : "contact"}
+      >
         <ContactIntroduction />
-        <Paperplane/>
+        <Paperplane />
         <ContactForm />
       </div>
     </motion.div>
