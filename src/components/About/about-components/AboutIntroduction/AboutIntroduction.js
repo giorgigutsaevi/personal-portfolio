8fe1d7bd159fc3dab14a8c3855681e03156d9ef2
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2026-02-28 12:54:13 +0000

    Update AboutIntroduction.js

--- src/components/About/about-components/AboutIntroduction/AboutIntroduction.js
+++ src/components/About/about-components/AboutIntroduction/AboutIntroduction.js
@@ -1,5 +1,4 @@
 import "./AboutIntroduction.css";
-import { Link } from "react-router-dom";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
