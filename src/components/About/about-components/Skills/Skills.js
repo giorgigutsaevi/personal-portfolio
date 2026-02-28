3df045740ba167410727856b356e3235ce021540
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2026-02-28 12:55:47 +0000

    Update Skills.js

--- src/components/About/about-components/Skills/Skills.js
+++ src/components/About/about-components/Skills/Skills.js
@@ -6,14 +6,14 @@ const Skills = () => {
       <div className="skills__title--main">My skills</div>
       <div className="skills__body--columns">
         <div className="skills__body--column">
-          Frontend, Backend, Test Driven Development, Object Oriented Design, debugging,
+          Cloud (AWS) Serverless Architecture, Frontend, Backend, CI/CD, Test Driven Development, Object Oriented Design, debugging,
           Agile, dabbling in UX/UI
         </div>
         <div className="skills__body--column column2">
-          JavaScript, React.JS, Node.JS, MongoDB, Express.JS, Ruby, Ruby on
-          Rails, Tailwind CSS, Bootstrap, GSAP, PostgreSQL, Matter.JS 
+          Lambda, Lex, Cognito, DynamoDB, IAM, S3, JavaScript, Python, React.JS, MongoDB, Express.JS, Ruby, Ruby on
+          Rails, Tailwind CSS, GSAP, PostgreSQL, Matter.JS 
         </div>
-        <div className="skills__body--column">Jest, Chai, Mocha, RSpec, React Testing Library</div>
+        <div className="skills__body--column">Vitest, Jest, Chai, Mocha, RSpec, React Testing Library</div>
 				<div className="skills__body--column colum4">
           Communication, Problem solving, Empathy, Fast-Learning,
           Pragmatism
